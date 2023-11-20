d8c57527886f6f7bc6c87d5fb43fdb3e852730b8
alechp <me@alechp.com>
2023-11-20 15:47:45 -0800

    initialized tailwind, added styles to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import qwikdev from "@qwikdev/astro";
-
 import node from "@astrojs/node";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [qwikdev()],
+  integrations: [qwikdev(), tailwind()],
   output: "server",
   adapter: node({
     mode: "standalone"
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,11 @@
 import { component$, useSignal } from "@builder.io/qwik";
- 
+
 export const Counter = component$(() => {
   const counter = useSignal(0);
- 
-  return <button onClick$={() => counter.value++}>{counter.value}</button>;
+  return (
+    <div>
+      <h1 class="text-red-300">Counter</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick$={() => counter.value++}>Value: {counter.value}</button>
+    </div>
+  );
 });
